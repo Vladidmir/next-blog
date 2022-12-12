73a3589de5f8b6f3ea9071dee30321f446cbbffa
Vladimir Skorobahatko <vladimirskor2305@gmail.com>
2022-12-12 16:38:05 +0200

    “admin-nav”

---
+++ components/layout/AdminLayout.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import { FC, ReactNode } from "react";
+import {
+  AiOutlineDashboard,
+  AiOutlineContainer,
+  AiOutlineTeam,
+  AiOutlineMail,
+  AiOutlineContacts,
+  AiOutlineFileAdd,
+} from "react-icons/ai";
+
+import AdminNav from "../common/AdminNav";
+
+interface IAdminLayoutProps {
+  children: ReactNode;
+}
+
+const navItems = [
+  { href: "/admin", icon: AiOutlineDashboard, label: "Dashboard" },
+  { href: "/admin/posts", icon: AiOutlineContainer, label: "Posts" },
+  { href: "/admin/users", icon: AiOutlineTeam, label: "Users" },
+  { href: "/admin/comments", icon: AiOutlineMail, label: "Comments" },
+  { href: "/admin/contact", icon: AiOutlineContacts, label: "Contact" },
+];
+
+const AdminLayout: FC<IAdminLayoutProps> = ({ children }): JSX.Element => {
+  return (
+    <div className="flex ">
+      <AdminNav navItems={navItems} />
+      <div className="flex-1 p-4">{children}</div>
+      {/* create button */}
+      <Link
+        className="bg-secondary-dark dark:bg-secondary-light text-primary dark:text-primary-dark fixed z-10 right-10 bottom-10 p-3 rounded-full hover:scale-90 shadow-sm transition"
+        href="/admin/post/create"
+      >
+        <AiOutlineFileAdd size={24} />
+      </Link>
+    </div>
+  );
+};
+
+export default AdminLayout;
