178ea23419f0e40d3c9bc8496663f070292a8492
Vladimir Skorobahatko <vladimirskor2305@gmail.com>
2022-12-20 16:59:09 +0200

    “editor_toolbar-updatingLink”

--- components/editor/Link/EditLink.tsx
+++ components/editor/Link/EditLink.tsx
@@ -1,13 +1,15 @@
-import { useCallback, FC } from "react";
+import { useCallback, useState, FC } from "react";
 import { BsBoxArrowUpRight, BsPencilSquare } from "react-icons/bs";
 import { BubbleMenu, Editor } from "@tiptap/react";
 import { BiUnlink } from "react-icons/bi";
+import LinkForm, { IlinkOption } from "./LinkForm";
 
 interface IEditLinkProps {
   editor: Editor;
 }
 
 const EditLink: FC<IEditLinkProps> = ({ editor }): JSX.Element => {
+  const [showEditForm, setShowEditForm] = useState(false);
   const handleOnLinkOpenClick = useCallback(() => {
     const { href } = editor.getAttributes("link");
     if (href) {
@@ -15,18 +17,45 @@ const EditLink: FC<IEditLinkProps> = ({ editor }): JSX.Element => {
     }
   }, [editor]);
 
-  const handleLinkEditClick = () => {};
+  const handleLinkEditClick = () => {
+    setShowEditForm(true);
+  };
 
   const handleUnlinkClick = () => {
     editor.commands.unsetLink();
   };
 
+  const handleSubmit = ({ url, openInNewTab }: IlinkOption) => {
+    editor
+      .chain()
+      .focus()
+      .unsetLink()
+      .setLink({ href: url, target: openInNewTab ? "_blank" : "" })
+      .run();
+    setShowEditForm(false);
+  };
+
+  const getInitialState = useCallback(() => {
+    const { href, target } = editor.getAttributes("link");
+    return { url: href, openInNewTab: target ? true : false };
+  }, [editor]);
+
   return (
     <BubbleMenu
       shouldShow={({ editor }) => editor.isActive("link")}
       editor={editor}
+      tippyOptions={{
+        onHide: () => {
+          setShowEditForm(false);
+        },
+      }}
     >
-      <div>
+      <LinkForm
+        visible={showEditForm}
+        onSubmit={handleSubmit}
+        initialState={getInitialState()}
+      />
+      {!showEditForm && (
         <div className="rounded bg-primary dark:bg-primary-dark text-primary-dark dark:text-primary shadow-secondary-dark shadow-md p-3 flex items-center space-x-6 z-50">
           <button onClick={handleOnLinkOpenClick}>
             <BsBoxArrowUpRight />
@@ -40,7 +69,7 @@ const EditLink: FC<IEditLinkProps> = ({ editor }): JSX.Element => {
             <BiUnlink />
           </button>
         </div>
-      </div>
+      )}
     </BubbleMenu>
   );
 };
