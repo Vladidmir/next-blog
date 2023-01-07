bbe34d3ab362a8b33379cbe47667784df0f2a620
Vladimir Skorobahatko <vladimirskor2305@gmail.com>
2023-01-07 18:13:44 +0200

    “editor_toolbar-SEO_form”

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from "next";
-import { useEffect, useState } from "react";
+import { useEffect, useState, ChangeEventHandler } from "react";
 import { EditorContent } from "@tiptap/react";
 
 import Tolbar from "./Toolbar/indext";
@@ -8,14 +8,35 @@ import GalleryModal from "./GalleryModal";
 import UseEditor from "./useEditor";
 
 import axios from "axios";
+import SEOForm, { ISEOResult } from "./SeoForm";
 
 interface IEditorProps {}
+
+export interface FinalPost extends ISEOResult {
+  title: string;
+  content: string;
+  thumbnail?: File | string;
+}
 
 const Editor: NextPage<IEditorProps> = ({}) => {
   const { editor, handleImageSelection } = UseEditor();
   const [showGallery, setShowGallery] = useState(false);
   const [images, setImages] = useState<{ src: string }[]>([]);
   const [uploading, setUploading] = useState(false);
+  const [post, setPost] = useState<FinalPost>({
+    title: "",
+    content: "",
+    meta: "",
+    tags: "",
+    slug: "",
+  });
+
+  const [seoInitialValue, setSeoInitialValue] = useState<ISEOResult>();
+  const updateSeoValue = (result: ISEOResult) =>
+    setPost({ ...post, ...result });
+
+  const updateTitle: ChangeEventHandler<HTMLInputElement> = ({ target }) =>
+    setPost({ ...post, title: target.value });
 
   const fetchImages = async () => {
     const { data } = await axios("/api/image");
@@ -40,12 +61,28 @@ const Editor: NextPage<IEditorProps> = ({}) => {
     <>
       <div className="p-3 dark:bg-primary-dark bg-primary transition">
         {editor && (
-          <Tolbar onShowGalerry={() => setShowGallery(true)} editor={editor} />
+          <>
+            <input
+              type="text"
+              className="py-2 outline-none bg-transparent w-full border-0 border-b-[1px] border-secondary-dark dark:border-secondary-light text-3xl font-semibold italic text-primary-dark dark:text-primary mb-3"
+              placeholder="Title"
+              onChange={updateTitle}
+              value={post.title}
+            />
+            <Tolbar
+              onShowGalerry={() => setShowGallery(true)}
+              editor={editor}
+            />
+          </>
         )}
         {editor ? <EditLink editor={editor} /> : null}
+        <EditorContent editor={editor} className="min-h-[300px]" />
         <div className="h-[1px] w-full bg-secondary-dark dark:bg-secondary-light my-3" />
-
-        <EditorContent editor={editor} />
+        <SEOForm
+          onChange={updateSeoValue}
+          title={post.title}
+          initialValue={seoInitialValue}
+        />
       </div>
 
       <GalleryModal
